171b2ffc56d1c9d97f9ba9dd9c57786214f6c9ba
tutannnn <koudai.nattoufu@gmail.com>
2024-06-10 16:04:37 +0900

    みす

--- Prog2Master/prog0810.cpp
+++ Prog2Master/prog0810.cpp
@@ -5,7 +5,7 @@ int prog8_10() {
     int a[10] = { 12,24,18,22,3,7,32,19,5,28 }, * p;
 
     p = a;
-    for (int i = 0; i < 5; i++,p++) {
+    for (int i = 0; i < 10; i++,p++) {
         if(*p>=10)printf("a[%d]:%f\n", i, sqrt((double)*p));
     }
     return 0;
